bd5eb7cb2af6e8ed40f1a39a14080af959c42ab5
Nomer Hapa <nomerhapa01@gmail.com>
2023-05-09 16:08:19 +0800

    .

--- 03-functions-scope/08iife/script.js
+++ 03-functions-scope/08iife/script.js
@@ -4,21 +4,21 @@
 //IFFE Syntax (Has its own scope and runs right away)
 
 (function () {
-  const user = 'Ryan';
+  const user = "Ryan";
   console.log(user);
 
-  const hello = () => console.log('Hello from the IFFE');
+  const hello = () => console.log("Hello from the IFFE");
   hello();
 })();
 
 //Params
 (function (name) {
-  console.log('Hello ' + name);
-})('Cat');
+  console.log("Hello " + name);
+})("Cat");
 
 //Named IIFE (can only be called recursively)
 (function hello() {
-  console.log('Hello');
+  console.log("Hello");
   //this is recursion dont do this  in this particular
   // hello();
 });
